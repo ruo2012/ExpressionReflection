d109170d57742d4558b6c1d7e720a2d46694c458
徐培堯 <st89382000@gmail.com>
2016-10-01 22:06:45 +0800

    加入針對GetMember的測試類別與方法

---
+++ ExpressionReflection.Test/Type_GetMemberExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressionReflection.Test {
+    [TestClass]
+    public class Type_GetMemberExtensionsTest {
+        [TestMethod]
+        public void GetMemberTest() {
+            List<int> test = new List<int>();
+            Assert.IsNotNull(
+                typeof(List<int>).GetMember<List<int>>(x => x.Count));
+            Assert.IsNotNull(
+                typeof(List<int>).GetMember(x => new List<int>()));
+            Assert.IsNotNull(
+                typeof(List<int>).GetMember<List<int>>(x => x.Clear()));
+            Assert.IsNotNull(
+                typeof(List<int>).GetMember<List<int>>(x => x.Sum()));
+        }
+    }
+}
